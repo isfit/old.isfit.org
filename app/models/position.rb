37dc04cdbe7bc53450d2d6112461db87af5478ff
iverds <iver.ds@gmail.com>
2010-10-13 00:23:18 +0200

    MMM

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,13 +1,14 @@
 class Position < ActiveRecord::Base
-	has_and_belongs_to_many :groups
-        belongs_to :group
+  has_and_belongs_to_many :groups
+  belongs_to :group
 
-        lang_attr :title, :description
+  lang_attr :title, :description
 
   def self.find_all_active_positions
     positions = Position.find_all_by_admission_id(3)
     positions.sort_by {|x| [(x.group.section == nil ? "" :  x.group.section.name_no), x.group.name_no] }
   end
+
   def self.find_all_active_positions_alfa #sorterer alfabetisk
     positions = Position.find_all_by_admission_id(3, :order => "title_no")
   end
