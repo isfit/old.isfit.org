37e17709014010d7d1e9e183d104e7c5a1d56d1b
Dag-Inge Aas <dagingaa@isfit.org>
2011-01-07 17:28:31 +0100

    Fikset eventssystem. Venter nå på layout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,32 +1,17 @@
 class EventsController < ApplicationController
- def index
-    @events = Event.find(:all)
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml { render :xml => @events }
+  def index
+    @categories = EventType.all
+    if not params[:category]
+      @events = Event.where(:visible => 1)
+    else
+      @category = EventType.where(:id => params[:category]).first
+      @events = @category.events
     end
-  end
-  
-  # GET /events/2010/8/1 
-  # GET /events/2010/8/1.xml
-  def showDate
-    @events = Event.find(:all, :conditions => "date LIKE '"+(Date.new(Integer(params[:year]), Integer(params[:month]), Integer(params[:day]))).to_s+"%'")
-	
-    respond_to do |format|
-      format.html # showDate.html.erb 
-      format.xml { render :xml => @events } 
-    end
+
   end
 
-  # GET /events/1
-  # GET /events/1.xml
   def show
     @event = Event.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml { render :xml => @events } 
-    end
   end
+
 end
---
+++ db/migrate/20110107135824_create_event_dates.rb
@@ -0,0 +1,12 @@
+class CreateEventDates < ActiveRecord::Migration
+  def self.up
+    create_table :event_dates do |t|
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :event_dates
+  end
+end
