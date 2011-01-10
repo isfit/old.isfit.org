817904637453a7eb4aa952b66e5b5e02fa392b12
Stian Fredrikstad <stianfr@isfit.org>
2011-01-10 22:08:53 +0100

    Added lineup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,15 +5,15 @@ class EventsController < ApplicationController
       @category = EventType.where(:id => params[:category]).first
       if params[:year] && params[:month] && params[:day]
         @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-        @events = @category.events.joins(:event_dates).all(:conditions=>"visible=1 AND event_dates.date LIKE '"+@date.to_s+"%'")
+        @events = @category.events.joins(:event_dates).all(:conditions=>"visible=1 AND event_dates.date LIKE '"+@date.to_s+"%' AND visible_at >= "+Date.new.to_s)
       else
-        @events = @category.events.where(:visible => 1)
+        @events = @category.events.where(:visible => 1).where("visible_at >= "+Date.new.to_s)
       end
     elsif params[:year] && params[:month] && params[:day]
       @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-      @events = Event.joins(:event_dates).all(:conditions=>"visible=1 AND event_dates.date LIKE '"+@date.to_s+"%'")
+      @events = Event.joins(:event_dates).all(:conditions=>"visible=1 AND event_dates.date LIKE '"+@date.to_s+"%' AND visible_at >= "+Date.new.to_s)
     else
-      @events = Event.where(:visible => 1)
+      @events = Event.where(:visible => 1).where("visible_at >= "+Date.new.to_s)
     end
 
   end
