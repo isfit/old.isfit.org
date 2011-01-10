65e538b1c09eb134a3bcbe4e1af3ec0a7aad07e5
Stian Fredrikstad <stianfr@isfit.org>
2011-01-11 00:58:47 +0100

    Fixed events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,21 +6,22 @@ class EventsController < ApplicationController
       @category = EventType.where(:id => params[:category]).first
       if params[:year] && params[:month] && params[:day]
         @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-        @events = @category.events.joins(:event_dates).all(:conditions=>"visible=1 AND event_dates.date LIKE '"+@date.to_s+"%' AND visible_at <= '"+now+"'")
+        @event_dates = @category.event_dates.joins(:event).all(:conditions=>"events.visible=1 AND event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"'")
       else
-        @events = @category.events.where(:visible => 1).where("visible_at <= '"+now+"'")
+        @event_dates = @category.event_dates.where("events.visible = 1").where("events.visible_at <= '"+now+"'")
       end
     elsif params[:year] && params[:month] && params[:day]
       @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-      @events = Event.joins(:event_dates).all(:conditions=>"visible=1 AND event_dates.date LIKE '"+@date.to_s+"%' AND visible_at <= '"+now+"'")
+      @event_dates = EventDate.joins(:event).all(:conditions=>"events.visible=1 AND event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"'")
     else
-      @events = Event.where(:visible => 1).where("visible_at <= '"+now+"'")
+      @event_dates = EventDate.joins(:event).where("events.visible = 1").where("events.visible_at <= '"+now+"'")
     end
 
   end
 
   def show
-    @event = Event.find(params[:id]).where("visible_at <= '"+now+"'").where(:visible => 1)
+    now = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    @event = EventDate.joins(:event).where(:id=>params[:event_date_id]).where("events.visible_at <= '"+now+"'").where("events.visible = 1").first
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ WwwIsfitOrg::Application.routes.draw do
 
     get "event/:id" => "events#show", :as => "event"
 
+    get "event/:id/:event_date_id" => "events#show", :as => "event"
     
     resources :pages
     resources :press_accreditations
