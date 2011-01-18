a2bcffc56a2eaab92ee0544898cba36b824b5102
iverds <iver.ds@gmail.com>
2011-01-18 08:23:26 +0100

    Fixed bug in events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,13 +8,13 @@ class EventsController < ApplicationController
       @category = EventType.where(:id => params[:category]).first
       if params[:year] && params[:month] && params[:day]
         @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-        @event_dates = @category.event_dates.joins(:event).all(:conditions=>"event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"'").order("event_dates.date")
+        @event_dates = @category.event_dates.joins(:event).where("event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"'")
       else
         @event_dates = @category.event_dates.where("events.visible_at <= '"+now+"'").order("event_dates.date")
       end
     elsif params[:year] && params[:month] && params[:day]
       @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-      @event_dates = EventDate.joins(:event).all(:conditions=>"event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"'").order("event_dates.date")
+      @event_dates = EventDate.joins(:event).where("event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"'")
     else
       @event_dates = EventDate.joins(:event).where("events.visible_at <= '"+now+"'").order("event_dates.date")
     end
