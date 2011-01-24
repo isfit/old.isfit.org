51bedce9e9b12eb276a478eb1a9783ed99ab456a
Stian Fredrikstad <stianfr@isfit.org>
2011-01-24 17:47:00 +0100

    Fixed deleted bug in events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,20 +8,20 @@ class EventsController < ApplicationController
       @category = EventType.where(:id => params[:category]).first
       if params[:year] && params[:month] && params[:day]
         @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-        @event_dates = @category.event_dates.joins(:event).where("event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"'")
+        @event_dates = @category.event_dates.joins(:event).where("event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"' AND events.deleted <> 1")
       else
-        @event_dates = @category.event_dates.where("events.visible_at <= '"+now+"'").order("event_dates.date")
+        @event_dates = @category.event_dates.where("events.visible_at <= '"+now+"'").order("event_dates.date AND events.deleted <> 1")
       end
     elsif params[:year] && params[:month] && params[:day]
       @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-      @event_dates = EventDate.joins(:event).where("event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"'")
+      @event_dates = EventDate.joins(:event).where("event_dates.date LIKE '"+@date.to_s+"%' AND events.visible_at <= '"+now+"' AND events.deleted <> 1")
     else
-      @event_dates = EventDate.joins(:event).where("events.visible_at <= '"+now+"'").order("event_dates.date")
+      @event_dates = EventDate.joins(:event).where("events.visible_at <= '"+now+"' AND events.deleted <> 1").order("event_dates.date")
     end
     @event_dates = @event_dates.order(:date)
     respond_to do |format|
       format.html
-      @events = Event.where("events.visible_at <= '"+now+"'") 
+      @events = Event.where("events.visible_at <= '"+now+"' AND events.deleted <> 1") 
       format.xml { render :xml => @events.to_xml(:include=>[:event_dates,:event_place,:event_type])}
       format.json { render :json => @events.to_xml(:include=>[:event_dates,:event_place,:event_type])}
    end
@@ -31,9 +31,9 @@ class EventsController < ApplicationController
   def show
     now = Time.now.strftime("%Y-%m-%d %H:%M:%S")
     if params[:event_date_id]
-      @event = EventDate.joins(:event).where(:id=>params[:event_date_id]).where("events.visible_at <= '"+now+"'").first
+      @event = EventDate.joins(:event).where(:id=>params[:event_date_id]).where("events.visible_at <= '"+now+"' AND events.deleted <> 1").first
     elsif
-      @event = EventDate.joins(:event).where("events.id = "+params[:id]+" AND events.visible_at <= '"+now+"'").first
+      @event = EventDate.joins(:event).where("events.id = "+params[:id]+" AND events.visible_at <= '"+now+"' AND events.deleted <> 1").first
     end
   
     respond_to do |format|
