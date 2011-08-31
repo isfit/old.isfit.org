b74ef0fa5e59ced64d2388b886a26b86553efd2c
Iver Dihle Skjervum <iver.ds@gmail.com>
2011-08-31 23:10:44 +0200

    Added a rocket

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'bundler/capistrano'
+
 set :application, "isfit.org"
 set :repository,  "git@github.com:isfit/isfit.org"
 
@@ -13,11 +15,17 @@ set :use_sudo, false
 set :deploy_to, "/srv/www/www.isfit.org"
 set :deploy_via, :remote_cache
 default_run_options[:pty] = true  # Must be set for the password prompt from git to work
+ssh_options[:forward_agent] = true
 
- namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
- end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :symlink_shared, :roles => :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+after 'deploy:update_code', 'deploy:symlink_shared'
+
