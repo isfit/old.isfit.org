ea55c85bad30c077e81745437358eb7c3132e80f
Iver Dihle Skjervum <iver.ds@gmail.com>
2011-10-20 10:39:46 +0200

    Mohaha

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -68,14 +68,14 @@ module ArticlesHelper
     return ""
     end
 
-    style = case type.to_s
+    style = case type
             when "1" then "photo_full"
             when "2" then "photo_right"
             when "3" then "photo_left"
             end
 
     img_text = Language.to_s == "en" ? picture.image_text_en : picture.image_text_no
-    pic_url = type == 1 ? picture.full_article_picture.url : picture.half_article_picture.url
+    pic_url = type == "1" ? picture.full_article_picture.url : picture.half_article_picture.url
     #Change on prod!
     if (link)
       url = "<a href=/assets/#{picture.original_picture.url}><div class=#{style}><img src =/assets/#{pic_url}  /><br /><i>Foto: #{picture.credits}</i><br /><i>#{picture.image_text_en}</i></div></a>"
