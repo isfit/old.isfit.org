6209bad834c37a95e3bf53c9ae0e8fe07d475214
Iver Dihle Skjervum <iver.ds@gmail.com>
2011-10-20 09:32:16 +0200

    Refactoring of helpers

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,6 @@
 require 'bluecloth'
 
 module ArticlesHelper
-
-
   def format(str)
     set_correct_tags(str)
     yt_tag(str)
@@ -20,52 +18,57 @@ module ArticlesHelper
   end
 
   def set_correct_tags(text)
-    while text.index('##pic') != nil
-      t_start = text.index('##pic')
-      t_end = text.index('pic##')
-      reg = Regexp.new(/##pic (\d+) (\d+) pic##/)
-      match = reg.match(text[t_start..t_end+4])
-      #img_id = text[t_start+6].chr
-      #img_type = text[t_start+8].chr
-      img_id = match[1]
-      img_type = match[2]
-      url = article_image(img_id.to_i, img_type.to_i, false)
-      text[t_start..t_end+4] = url
-    end
+#    while text.index('##pic') != nil
+#      t_start = text.index('##pic')
+#      t_end = text.index('pic##')
+#      reg = Regexp.new(/##pic (\d+) (\d+) pic##/)
+#        match = reg.match(text[t_start..t_end+4])
+#      #img_id = text[t_start+6].chr
+#      #img_type = text[t_start+8].chr
+#      img_id = match[1]
+#      img_type = match[2]
+#      url = article_image(img_id.to_i, img_type.to_i, false)
+#      text[t_start..t_end+4] = url
+#    end
+    text.gsub!(/##pic (\d+) (\d+) pic##/, article_image("\\1".to_i, "\\2".to_i, false))
 
-    while text.index('#l#pic') != nil
-      t_start = text.index('#l#pic')
-      t_end = text.index('pic#l#')
-      reg = Regexp.new(/#l#pic (\d+) (\d+) pic#l#/)
-      match = reg.match(text[t_start..t_end+5])
-      #img_id = text[t_start+6].chr
-      #img_type = text[t_start+8].chr
-      img_id = match[1]
-      img_type = match[2]
-      url = article_image(img_id.to_i, img_type.to_i, true)
-      text[t_start..t_end+5] = url
-    end 
+    text.gsub!(/#l#pic (\d+) (\d+) pic#l#/, article_image("\\1".to_i, "\\2".to_i, true))
+      #    while text.index('#l#pic') != nil
+      #      t_start = text.index('#l#pic')
+      #      t_end = text.index('pic#l#')
+      #      reg = Regexp.new(/#l#pic (\d+) (\d+) pic#l#/)
+      #      match = reg.match(text[t_start..t_end+5])
+      #      #img_id = text[t_start+6].chr
+      #      #img_type = text[t_start+8].chr
+      #      img_id = match[1]
+      #      img_type = match[2]
+      #      url = article_image(img_id.to_i, img_type.to_i, true)
+      #      text[t_start..t_end+5] = url
+      #    end 
   end
 
   def yt_tag(text)
-    while text.index('##yt') != nil
-      t_start = text.index('##yt')
-      t_end = text.index('yt##')
-      reg = Regexp.new(/##yt (\w+) yt##/)
-      match = reg.match(text[t_start..t_end+3])
-      yt_id = match[1]
-      url = "<iframe width='530' height='302' src='http://www.youtube.com/embed/#{yt_id}' frameborder='0' allowfullscreen></iframe>"
-      text[t_start..t_end+3] = url
-    end
+    text.gsub!(/##yt (\w+) yt##/, "<iframe width='530' height='302' src='http://www.youtube.com/embed/\\1' frameborder='0' allowfullscreen></iframe>")
+
+      #    while text.index('##yt') != nil
+      #      t_start = text.index('##yt')
+      #      t_end = text.index('yt##')
+      #      reg = Regexp.new(/##yt (\w+) yt##/)
+      #      match = reg.match(text[t_start..t_end+3])
+      #      yt_id = match[1]
+      #      url = "<iframe width='530' height='302' src='http://www.youtube.com/embed/#{yt_id}' frameborder='0' allowfullscreen></iframe>"
+      #      text[t_start..t_end+3] = url
+      #    end
   end
 
   def article_image(picture_id, type, link)
     picture = Photo.find_by_id(picture_id)
+    return "" unless picture
     style = case type.to_s
-      when "1" then "photo_full"
-      when "2" then "photo_right"
-      when "3" then "photo_left"
-    end
+            when "1" then "photo_full"
+            when "2" then "photo_right"
+            when "3" then "photo_left"
+            end
 
     img_text = Language.to_s == "en" ? picture.image_text_en : picture.image_text_no
     pic_url = type == 1 ? picture.full_article_picture.url : picture.half_article_picture.url
@@ -75,5 +78,5 @@ module ArticlesHelper
     else
       url = "<div class=#{style}><img src =/assets/#{pic_url}  /><br /><i>Foto: #{picture.credits}</i><br /><i>#{picture.image_text_en}</i></div>"
     end 
- end
+  end
 end
