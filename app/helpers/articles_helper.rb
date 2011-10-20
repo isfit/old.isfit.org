4f050e69cadaf3aa4f8e53d6f98f866d39626c70
Iver Dihle Skjervum <iver.ds@gmail.com>
2011-10-20 10:15:06 +0200

    Refactoring again...

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -18,33 +18,33 @@ module ArticlesHelper
   end
 
   def set_correct_tags(text)
-    while text.index('##pic') != nil
-      t_start = text.index('##pic')
-      t_end = text.index('pic##')
-      reg = Regexp.new(/##pic (\d+) (\d+) pic##/)
-        match = reg.match(text[t_start..t_end+4])
-      #img_id = text[t_start+6].chr
-      #img_type = text[t_start+8].chr
-      img_id = match[1]
-      img_type = match[2]
-      url = article_image(img_id.to_i, img_type.to_i, false)
-      text[t_start..t_end+4] = url
-    end
-    #text.gsub!(/##pic (\d+) (\d+) pic##/, article_image("\\1".to_i, "\\2".to_i, false))
+#    while text.index('##pic') != nil
+#      t_start = text.index('##pic')
+#      t_end = text.index('pic##')
+#      reg = Regexp.new(/##pic (\d+) (\d+) pic##/)
+#        match = reg.match(text[t_start..t_end+4])
+#      #img_id = text[t_start+6].chr
+#      #img_type = text[t_start+8].chr
+#      img_id = match[1]
+#      img_type = match[2]
+#      url = article_image(img_id.to_i, img_type.to_i, false)
+#      text[t_start..t_end+4] = url
+#    end
+    text.gsub!(/##pic (\d+) (\d+) pic##/, article_image('\1', '\2', false))
 
-    #text.gsub!(/#l#pic (\d+) (\d+) pic#l#/, article_image("\\1".to_i, "\\2".to_i, true))
-          while text.index('#l#pic') != nil
-            t_start = text.index('#l#pic')
-            t_end = text.index('pic#l#')
-            reg = Regexp.new(/#l#pic (\d+) (\d+) pic#l#/)
-            match = reg.match(text[t_start..t_end+5])
-            #img_id = text[t_start+6].chr
-            #img_type = text[t_start+8].chr
-            img_id = match[1]
-            img_type = match[2]
-            url = article_image(img_id.to_i, img_type.to_i, true)
-            text[t_start..t_end+5] = url
-          end 
+    text.gsub!(/#l#pic (\d+) (\d+) pic#l#/, article_image("\\1".to_i, "\\2".to_i, true))
+ #         while text.index('#l#pic') != nil
+ #           t_start = text.index('#l#pic')
+ #           t_end = text.index('pic#l#')
+ #           reg = Regexp.new(/#l#pic (\d+) (\d+) pic#l#/)
+ #           match = reg.match(text[t_start..t_end+5])
+ #           #img_id = text[t_start+6].chr
+ #           #img_type = text[t_start+8].chr
+ #           img_id = match[1]
+ #           img_type = match[2]
+ #           url = article_image(img_id.to_i, img_type.to_i, true)
+ #           text[t_start..t_end+5] = url
+ #         end 
   end
 
   def yt_tag(text)
