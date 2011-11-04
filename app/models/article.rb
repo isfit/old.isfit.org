ce938511d61832883824ccdb28e6de4ba54d2fd1
Dag-Inge Aas <daginge@gmail.com>
2011-11-04 20:12:42 +0100

    Fixed support for new and old images in articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ActiveRecord::Base
   set_primary_key 'id'
   lang_attr :title, :ingress, :body, :sub_title
+
+  has_attached_file :frontend_article_image, styles:  {
+    front_large: {geometry: "530x196#", processors: [:cropper] },
+    front_small: {geometry: "250x120#", processors: [:cropper_half] },
+    article: {geometry: "447x346#", processors: [:cropper_spp_one_third] }
+  }
+
 end
