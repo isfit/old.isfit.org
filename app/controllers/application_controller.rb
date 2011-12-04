325cdbf50c8eda90fac341c1e761192928f8bfdb
Dag-Inge Aas <daginge@gmail.com>
2011-12-04 20:07:35 +0100

    Fixed location check and removed pointless days_until method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,8 @@ class ApplicationController < ActionController::Base
   helper :all
 
   protect_from_forgery
-  before_filter  :set_language, :set_days_until
+  before_filter  :set_language
 
-  def set_days_until
-  difference = (Time.local(2010,"sep",30,23,59,59)-Time.now.getlocal)
-
-  @seconds    =  difference % 60
-  difference = (difference - @seconds) / 60
-  @minutes    =  difference % 60
-  difference = (difference - @minutes) / 60
-  @hours      =  difference % 24
-  difference = (difference - @hours)   / 24
-  @days       =  difference % 7
-  end
-
-  
   def set_language
     unless session[:locale]
       country_code = get_country_code_by_id(request.remote_ip)
@@ -41,7 +28,7 @@ class ApplicationController < ActionController::Base
       Language.set(params[:locale])
       I18n.locale = params[:locale]
       session[:locale] = Language.to_s
-      redirect_to :back
+      redirect_to request.env["HTTP_REFERER"] || root_path
     end
   end
 
