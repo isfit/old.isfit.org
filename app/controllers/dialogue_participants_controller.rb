cfa05daf0532d66bfee925fa7f9248f0bfbdcfa6
Dag-Inge Aas <daginge@gmail.com>
2012-04-28 10:16:07 +0200

    Fixed dialogue participants

--- app/controllers/dialogue_participants_controller.rb
+++ app/controllers/dialogue_participants_controller.rb
@@ -1,37 +1,38 @@
 class DialogueParticipantsController < ApplicationController
   def index
-  	redirect_to :action => "new"
+    redirect_to :action => "new"
   end
 
   def new
-	@countries = Country.find(:all, :order=>"name")
+    @countries = Country.find(:all, :order=>"name")
+    @dialogue_participant = DialogueParticipant.new
+  end
 
-	if request.post?
+  def create
+    @countries = Country.find(:all, :order=>"name")
+    begin
+      @dialogue_participant = DialogueParticipant.new(params[:dialogue_participant])
 
-		begin
-			@dialogue_participant = DialogueParticipant.new(params[:dialogue_participant])
+      # Catch the exception from AR::Base
+    rescue ActiveRecord::MultiparameterAssignmentErrors => ex
+      # Iterarate over the exceptions and remove the invalid field components from the input
+      ex.errors.each { |err| params[:dialogue_participant].delete_if { |key, value| key =~ /^#{err.attribute}/ } }
+      # Recreate the Model with the bad input fields removed
 
-		# Catch the exception from AR::Base
-		rescue ActiveRecord::MultiparameterAssignmentErrors => ex
-			# Iterarate over the exceptions and remove the invalid field components from the input
-			ex.errors.each { |err| params[:dialogue_participant].delete_if { |key, value| key =~ /^#{err.attribute}/ } }
-			# Recreate the Model with the bad input fields removed
-
-			@dialogue_participant = Participant.new(params[:dialogue_participant])
-		end
-		@dialogue_participant.registered_time = DateTime.now
-		if @dialogue_participant.valid? && verify_recaptcha(:model=>@dialogue_participant, :message=>"Recaptcha verification failed") && @dialogue_participant.save
-			Postoffice.registered(@dialogue_participant.first_name + " " + @dialogue_participant.last_name, @dialogue_participant.email).deliver
-			flash[:notice] = "Your application was sent successfully. You should receive an email as a confirmation at the email you provided"
-                        render :action => "new"
-		else
+      @dialogue_participant = Participant.new(params[:dialogue_participant])
+    end
+    @dialogue_participant.registered_time = DateTime.now
+    if @dialogue_participant.valid? && verify_recaptcha(:model=>@dialogue_participant, :message=>"Recaptcha verification failed") && @dialogue_participant.save
+      Postoffice.registered(@dialogue_participant.first_name + " " + @dialogue_participant.last_name, @dialogue_participant.email).deliver
+      flash[:notice] = "Your application was sent successfully. You should receive an email as a confirmation at the email you provided"
+      render :action => "new"
+    else
       verify_recaptcha(:model=>@dialogue_participant, :message=>"Recaptcha verification failed")
       flash[:notice] = nil
-			#validate_recap(params, @dialogue_participant.errors)
-			#flash[:warnings] = @dialogue_participant.errors
-		end
-  else
-  @dialogue_participant = DialogueParticipant.new
-	end
+      #validate_recap(params, @dialogue_participant.errors)
+      #flash[:warnings] = @dialogue_participant.errors
+    end
+    render tab: params[:tab], action: "new"
+
   end
 end
