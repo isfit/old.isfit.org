108313f07262481ff8a5d29959e2be47ba11f69a
Erik Bergersen <bergersen.erik@gmail.com>
2012-08-07 13:34:12 +0200

    Removed unused functions in position model

--- app/models/position.rb
+++ app/models/position.rb
@@ -9,16 +9,6 @@ class Position < ActiveRecord::Base
     self.groups.first
   end
 
-  def self.find_all_active_positions
-    positions = Position.find_all_by_admission_id(5)
-    #positions.sort_by {|x| [(x.group.section == nil ? "" :  x.group.section.name_no), x.group.name_no] }
-    positions
-  end
-
-  def self.find_all_active_positions_alfa #sorterer alfabetisk
-    positions = Position.where(:admission_id => 5).order("title_no ASC").all
-  end
-
   scope :published, lambda { where("publish_from < ? AND publish_to > ?", Time.zone.now, Time.zone.now) }
 
   def select_name
