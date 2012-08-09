241e09e3e8845fad47eb5656d2683f73b9172bc1
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2012-08-09 13:15:00 +0200

    Add your own ideas, + more javascript hooks

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,11 +1,20 @@
 class IdeasController < ApplicationController
   def index
-    @ideas = Idea.all
+    @idea  = Idea.new
+    @ideas = Idea.all(:order => "created_at DESC")
   end
 
   def show
+    @idea = Idea.find(params[:id])
   end
 
   def create
+    @idea = Idea.new(params[:idea])
+    if @idea.save
+      redirect_to ideas_path, :notice => 'Huge success!'
+    else
+      redirect_to ideas_path, :notice => 'Major defeat!'
+    end
+
   end
 end
