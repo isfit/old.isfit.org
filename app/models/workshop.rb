99b34ad2b33cf5128b877845833030d7d8c85eab
Erik Bergersen <bergersen.erik@gmail.com>
2012-08-09 14:26:09 +0200

    Fixed errors in workshop

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,3 +1,9 @@
 class Workshop < ActiveRecord::Base
-  lang_attr :title, :ingress, :body, :sub_title
+  #lang_attr :title, :ingress, :body, :sub_title
+
+  attr_accessor :image_file_name, :image_content_type, :image_file_size, :image_updated_at
+
+  has_attached_file :workshop_image, :styles => { :original => "250x530>",
+                                                  :thumb => "120x230>"}
+
 end
