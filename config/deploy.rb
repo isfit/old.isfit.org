f6ac0b3dbd1bc56fbe07f8e04c69a0592233ae9b
Dag-Inge Aas <daginge@gmail.com>
2012-08-14 02:12:53 +0200

    update capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,10 +15,10 @@ role :web, "nova.isfit.org"                           # Your HTTP server, Apache
 role :app, "nova.isfit.org"                           # This may be the same as your `Web` server
 role :db,  "nova.isfit.org", :primary => true         # This is where Rails migrations will run
 
-set :branch, "v3-wip"
+set :branch, "master"
 set :user, "passenger"
 set :use_sudo, false
-set :deploy_to, "/srv/www/beta.isfit.org"
+set :deploy_to, "/srv/www/www.isfit.org"
 set :deploy_via, :remote_cache
 default_run_options[:pty] = true  # Must be set for the password prompt from git to work
 ssh_options[:forward_agent] = true
