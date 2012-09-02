8cdbdd54c73826ba349795ecd8128b5615938c1a
Erik Bergersen <bergersen.erik@gmail.com>
2012-09-02 15:50:52 +0200

    Postback to paypal works, sandbox only

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,3 +1,6 @@
+require "uri"
+require "net/http"
+
 class DonationsController < ApplicationController
   protect_from_forgery :except => [:create]
 
@@ -7,9 +10,7 @@ class DonationsController < ApplicationController
   end
 
   def thank_you
-    if params[:payment_status] == "Completed"
-
-    else
+    if params[:payment_status] != "Completed"
       redirect_to page_path(60)
     end
   end
@@ -17,7 +18,7 @@ class DonationsController < ApplicationController
   def create
     @exist = Donation.find_by_transaction_number(params[:txn_id])
 
-    if params[:payment_status] == "Completed" && @exist.nil?
+    if params[:payment_status] == "Completed" && @exist.nil? && verify?(params)
       Donation.create!(:status => params[:payment_status], 
                        :transaction_number => params[:txn_id], 
                        :amount => params[:payment_gross],
@@ -28,4 +29,24 @@ class DonationsController < ApplicationController
    
     render :nothing => true
   end
+
+  private
+
+  def verify?(params)
+    paypal_uri = URI.parse("https://www.sandbox.paypal.com/cgi-bin/webscr")
+    http = Net::HTTP.new(paypal_uri.host, paypal_uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Post.new(paypal_uri.request_uri)
+    request.set_form_data(params.merge("cmd" => "_notify-validate"))
+    response = http.request(request)
+
+    if response.body == "VERIFIED"
+      true
+    else
+      false
+    end
+  end
+
 end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -4,7 +4,6 @@ class Donation < ActiveRecord::Base
       :hosted_button_id => 'P898G78SELH86',
       :cmd => '_s-xclick',
       :return => return_url,
-      :invoice => '50',
       :notify_url => notify_url
     }
     "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
