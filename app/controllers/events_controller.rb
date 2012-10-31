3a3e75d18e7a8881bccf6229c9de1bcfbcb28ce8
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2012-10-31 20:17:08 +0100

    further development on the event prototype. bonus feature - checks samfundet for ticket availability

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,25 @@
-class EventsController < ApplicationController
+class EventsController < ApplicationController 
+
   def index
     @events = Event.all
   end
 
   def show
+    require 'open-uri'
+    require 'json'
+
     @event = Event.find(params[:id])
+
+    # @token = '111446192243028'
+    # url = URI.parse('https://graph.facebook.com/510373698975140')
+
+    # result = open(url)
+
+    # puts '---------'
+    # puts result
+    # puts '---------'
+
+    #@resultat = resultat
+
   end
 end
