14790657253c3ab1f51a12628703237d8f1c7208
Dag-Inge Aas <daginge@gmail.com>
2013-02-15 16:07:26 +0100

    remove caching, we probably dont need it

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,7 +1,5 @@
 class MarketingController < ApplicationController
 
-  caches_page :frontpage
-
   def frontpage
     @articles = Article.where("(show_article <='"+Time.now.strftime("%Y-%m-%d %H:%M:%S")+"' OR show_article IS NULL)").where(deleted: 0).where(list: 1).order("weight DESC").limit(5)
     if Language.to_s =="en"
