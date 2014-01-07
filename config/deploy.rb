1bf98f72745b00663fd26210c3c213d31d203c2b
Kristian Våge <krisvaage@gmail.com>
2014-01-07 16:19:08 +0100

    Add staging server to capistrano, 'cap production deploy' replaces cap deploy and cap staging deploy deploys to staging.isfit.org with 'cap staging deploy' using a copy of the local changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,10 +18,20 @@ role :db,  "nova.isfit.org", :primary => true         # This is where Rails migr
 set :branch, "master"
 set :user, "passenger"
 set :use_sudo, false
-set :deploy_to, "/srv/www/www.isfit.org"
-set :deploy_via, :remote_cache
+
 default_run_options[:pty] = true  # Must be set for the password prompt from git to work
 ssh_options[:forward_agent] = true
+
+task :production do
+  set :deploy_to, "/srv/www/www.isfit.org"
+  set :deploy_via, :remote_cache
+end
+
+task :staging do
+  set :deploy_to, "/srv/www/staging.isfit.org"
+  set :deploy_via, :copy
+end
+
 
 namespace :deploy do
   task :start do ; end
