81087bb44f365637cf62c2c535b0df8ed088b4ab
Kristian Våge <krisvaage@gmail.com>
2014-01-20 07:18:28 +0100

    Layout fixes

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,38 +1,79 @@
+# encoding: utf-8
 class PositionsController < ApplicationController
-  # coding:utf-8
  def index
     #@positions = Position.published.includes(:groups).order("groups.section_id, groups.id, positions.title_no")
     @positions = Position.all_ordered_by_section_name_position_name
   end
  
- def show
-   @position = Position.find_by_id(params[:id])
-   #unless Position.published.all.include?(@position)
-   #  redirect_to opptak_path, :notice => "That position does not exist"
-   #end
- end
+  def show
+    @position = Position.find_by_id(params[:id])
+    #unless Position.published.all.include?(@position)
+    #  redirect_to opptak_path, :notice => "That position does not exist"
+    #end
+  end
 
- def section
-   @section = Section.find(params[:id])
+  def edit
+   @applicant = Applicant.find(params[:id])
+   @positions_collection = positions_collected
+  end
 
-   @positions = @section.positions
- end
+  def update
+    @applicant = Applicant.find(current_user.applicant.id)
 
- def apply
-   @applicant = Applicant.new
-   @positions = Position.published.includes(:groups).order("groups.section_id, groups.id,positions.title_no")
-   @positions_collection = []
-   @positions.each { |p| @positions_collection << ["#{p.groups.first.name} - #{p.title}", p.id]} 
- end
+    respond_to do |format|
+      if @applicant.update_attributes(params[:applicant])
+        format.html { redirect_to show_applicant_user_path, notice: 'Søknad oppdatert.' }
+      else
+        format.html { render action: "edit" }
+      end
+    end
+  end
+
+  def section
+  @section = Section.find(params[:id])
+
+  @positions = @section.positions
+end
+
+def apply
+  @applicant = Applicant.new
+  @positions_collected = positions_collected
+end
 
  def save
     @applicant = Applicant.new(params[:applicant])
+    new_user = false
+
+    if current_user
+      @applicant.applicant_user_id = current_user.id
+    else
+      new_user = true
+      @applicant_user = ApplicantUser.new({ mail: @applicant.mail })
+      @password = @applicant_user.set_password
+      if @applicant_user.save
+        @applicant.applicant_user_id = @applicant_user.id
+      else
+        flash[:notice] = "Noe gikk galt. Har du allerede registrert med denne epostadressen? Hvis ikke kontakt orakel@isfit.org på epost."
+        render action: :apply
+        return
+      end
+    end
+
     respond_to do |format|
       if @applicant.save
-        Postoffice.applicant_add(@applicant.firstname + " " + @applicant.lastname, @applicant.mail).deliver
-        flash[:notice] = "Din soknad ble sendt."
+        if new_user
+          Postoffice.applicant_add_with_new_user(@applicant.full_name, @applicant.mail, @password).deliver
+        else
+          Postoffice.applicant_add(@applicant.full_name, @applicant.mail).deliver
+        end
+        flash[:notice] = "Din søknad ble sendt."
         @positions = Position.published
-        format.html { render :action => :index }
+
+        if current_user.nil?
+          session[:user_id] = @applicant_user.id
+        end
+
+        format.html { redirect_to show_applicant_user_path }
       else
         flash[:notice] = nil
         @positions = Position.published
@@ -48,4 +89,11 @@ class PositionsController < ApplicationController
      render :json => @valid
    end
  end
+end
+
+private
+def positions_collected
+  @positions = Position.published.includes(:groups).order("groups.section_id, groups.id,positions.title_no")
+  @positions_collection = []
+  @positions.each { |p| @positions_collection << ["#{p.groups.first.name} - #{p.title}", p.id]} 
 end
