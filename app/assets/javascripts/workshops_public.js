b0e0d32159cab87b996dc45edde7334e41a688c0
chrpeter <christian150790@gmail.com>
2014-05-05 19:16:01 +0200

    fixes som bugs and random size on circles

--- app/assets/javascripts/workshops_public.js
+++ app/assets/javascripts/workshops_public.js
@@ -1,19 +1,41 @@
 $(document).ready(function(){
+
+    $('body aside').remove();
+    $('#content').attr('class', 'span12');
+
+    
+
 	$( ".ch-item" ).each(function(){
 
     var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
 
-    
+
+   var rank = 1/$(this).attr('rank');
+   var width = $(this).width() * rank;
+   var height = $(this).height() * rank;
+   var width1 = $(this).parent().width() * rank;
+   var height1 = $(this).parent().height() * rank;
+
+    $(this).parent().css({
+        'height': height1,
+        'width': width1    
+    });
+
     var posx = (Math.random() * ($('.mainsection').width() - 200));
     var posy = (Math.random() * ($('.mainsection').height() - 200));
     
     $(this).css({
-        'left':posx+'px',
-        'top':posy+'px',
-        'background-color': color
+        //'left':posx+'px',
+        //'margin-top':posy+'px',
+        'background-color': color,
+        'height': height,
+        'width': width
 
     });
+    posx = 0;
+    posy = 0;
 	});
+
         
     }); 
 
