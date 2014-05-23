ecfc0f2bc17fd05806b5854275fb65af94ba0a04
Kristian Våge <krisvaage@gmail.com>
2014-05-23 17:37:28 +0200

    Added world tour notice on frontpage

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class MarketingController < ApplicationController
 
   def frontpage
@@ -5,10 +6,24 @@ class MarketingController < ApplicationController
 
     @stream = Stream.where("(publish_at <='"+Time.now.strftime("%Y-%m-%d %H:%M:%S")+"' AND end_at >= '" + Time.now.strftime("%Y-%m-%d %H:%M:%S")+"')").limit(1)[0]
 
+    flash[:notice] = world_tour_notice
     render layout: "application_no_boxes"
   end
 
   def world_map
     render layout: "application_clean"
+  end
+
+  private
+  def world_tour_notice
+    if Language.to_s.eql?("en")
+      "<p style='text-align: center'>ISFiT is now on a World Tour. " +
+      "Read more about it on the <a href=\"http://isfit-ambassadors.blogspot.no\">" +
+      "ISFiT Ambassador blog</a>.</p>"
+    else
+      "<p style='text-align: center'>ISFiT er på verdensturné. " +
+      "Les mer på <a href=\"http://isfit-ambassadors.blogspot.no\">" +
+      "ISFiT Ambassadør-bloggen</a>.</p>"
+    end
   end
 end
